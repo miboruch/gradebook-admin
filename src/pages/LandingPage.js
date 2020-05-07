45eb75c29b440717c9bcacee267908291d047b14
Michał Boruch <miboruch@gmail.com>
2020-05-07 13:50:50 +0200

    Project structure

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Lading page</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
