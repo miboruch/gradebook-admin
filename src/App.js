06b15603dca19b620be44c9504d5d9e4949363ac
Michał Boruch <miboruch@gmail.com>
2020-05-07 14:03:54 +0200

    Added svg animation and formik

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LandingPage from './pages/LandingPage';
 import SEO from './components/SEO';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './style/theme';
+import GlobalStyle from './style/GlobalStyle';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className='App'>
         <SEO />
+        <GlobalStyle />
         <LandingPage />
       </div>
     </ThemeProvider>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,84 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Formik, Form } from 'formik';
+import Input from '../components/Input/Input';
+import LoginScene from '../components/LoginScene/LoginScene';
+
+const StyledWrapper = styled.section`
+  width: 90%;
+  height: 95vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledForm = styled(Form)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+
+  ${({ theme }) => theme.mq.tablet} {
+    width: 50%;
+  }
+`;
+
+const StyledHeading = styled.h1`
+  font-size: 52px;
+  letter-spacing: 1px;
+  color: #2d2d2d;
+`;
+
+const StyledButton = styled.button`
+  width: 250px;
+  height: 40px;
+  margin-top: 3rem;
+  border: none;
+  background-color: #2eae83;
+  color: #fff;
+  font-family: 'Gilroy';
+  font-size: 14px;
+  letter-spacing: 2px;
+  font-weight: 500;
+  border-radius: 20px;
+  cursor: pointer;
+`;
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Lading page</h1>
-    </div>
+    <StyledWrapper>
+      <Formik
+        initialValues={{ login: '', password: '' }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ values, handleChange, handleBlur }) => (
+          <StyledForm>
+            <StyledHeading>Login</StyledHeading>
+            <Input
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+              type={'text'}
+              value={values.email}
+              name={'login'}
+              placeholder={'login'}
+            />
+            <Input
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+              type={'password'}
+              value={values.password}
+              name={'password'}
+              placeholder={'Password'}
+            />
+            <StyledButton>Submit</StyledButton>
+          </StyledForm>
+        )}
+      </Formik>
+      <LoginScene />
+    </StyledWrapper>
   );
 };
 
