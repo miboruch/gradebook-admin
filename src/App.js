2f98b67b7b0e18ba16c5978e2dc3ce5667a852c9
Michał Boruch <miboruch@gmail.com>
2020-05-24 17:12:24 +0200

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import SEO from './components/SEO';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './style/theme';
 import GlobalStyle from './style/GlobalStyle';
 import { fetchCourses, fetchUniversities } from './actions/mainActions';
+import AccountsPage from './pages/AccountsPage';
 
 function App({ fetchUniversities, fetchCourses }) {
   useEffect(() => {
@@ -15,11 +17,14 @@ function App({ fetchUniversities, fetchCourses }) {
   }, []);
   return (
     <ThemeProvider theme={theme}>
-      <div className='App'>
-        <SEO />
-        <GlobalStyle />
-        <LandingPage />
-      </div>
+      <SEO />
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route exact path={'/'} component={LandingPage} />
+          <Route exact path={'/accounts'} component={AccountsPage} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ src/pages/AccountsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AccountsPage = () => {
+  return (
+    <div>
+      <h1>HELLO FRIEND</h1>
+      <h1>HELLO FRIEND</h1>
+    </div>
+  );
+};
+
+export default AccountsPage;
